d76c232612c5768ddf9a8888028fe9cf3522ca77
yingranbc <71673372+yingranbc@users.noreply.github.com>
2020-10-02 19:11:19 -0400

    test

--- RunThisFirst.sh
+++ RunThisFirst.sh
@@ -1,4 +1,8 @@
+source /srv/conda/bin/activate kernel
+exec "$@"
+
 jupyter contrib nbextension install --sys-prefix
 jupyter nbextension enable equation-numbering/main --sys-prefix
 
 jupyter serverextension enable --py nbgitpuller --sys-prefix
+
